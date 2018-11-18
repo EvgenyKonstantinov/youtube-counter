e0764801af7ac4094de302696df85c872fd70a59
EvgenyKonstantinov <mr.ken87@yandex.ru>
2018-11-18 04:31:37 +0300

    Написал асинхронный Ajax запрос на сервер

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
-'use strict';
+"use strict";
 
-var name = 'Evgeny';
+var HttpClient = function HttpClient() {
+	this.get = function (aUrl, aCallback) {
+		var anHttpRequest = new XMLHttpRequest();
+		anHttpRequest.onreadystatechange = function () {
+			if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200) aCallback(anHttpRequest.responseText);
+		};
+
+		anHttpRequest.open("GET", aUrl, true);
+		anHttpRequest.send(null);
+	};
+};
+
+var client = new HttpClient();
+var showStat = function showStat() {
+	client.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCWNQKepV4st7cQTpDdykYqw&key=AIzaSyA2CVFfCLdJu1y9pUfQJ7Osjexm5GAsQyo', function (response) {
+		console.log(JSON.parse(response).items[0].statistics.subscriberCount);
+	});
+};
+
+showStat();
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,44 @@
-let name = 'Evgeny';
+var HttpClient = function () {
+	this.get = function (aUrl, aCallback) {
+		let anHttpRequest = new XMLHttpRequest();
+		anHttpRequest.onreadystatechange = function () {
+			if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+				aCallback(anHttpRequest.responseText);
+		}
+
+		anHttpRequest.open("GET", aUrl, true);
+		anHttpRequest.send(null);
+	}
+}
+
+let client = new HttpClient();
+let showStat = () => {
+	client.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCWNQKepV4st7cQTpDdykYqw&key=AIzaSyA2CVFfCLdJu1y9pUfQJ7Osjexm5GAsQyo', (response) => {
+		console.log(JSON.parse(response).items[0].statistics.subscriberCount);
+	});
+}
+
+
+
+
+
+
+// // 1. Создаём новый объект XMLHttpRequest
+// var xhr = new XMLHttpRequest();
+
+// // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
+// xhr.open('GET', 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCWNQKepV4st7cQTpDdykYqw&key=AIzaSyA2CVFfCLdJu1y9pUfQJ7Osjexm5GAsQyo', false);
+
+// // 3. Отсылаем запрос
+// xhr.send();
+
+// // 4. Если код ответа сервера не 200, то это ошибка
+// if (xhr.status != 200) {
+//   // обработать ошибку
+//   console.log( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+// } else {
+// 	// вывести результат
+// 	var subsCount = JSON.parse(xhr.responseText ).items[0].statistics.subscriberCount;
+// 	document.querySelector('#count').innerText = subsCount;
+//    // responseText -- текст ответа.
+// }
