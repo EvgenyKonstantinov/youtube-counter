b15120f1a3f8cf858da44fae97bda4428c0b3728
EvgenyKonstantinov <mr.ken87@yandex.ru>
2018-11-19 23:17:27 +0300

    commit#2

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,10 +13,9 @@ var HttpClient = function HttpClient() {
 };
 
 var client = new HttpClient();
+var idChannel = 'UCVswRUcKC-M35RzgPRv8qUg';
 var showStat = function showStat() {
-	client.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCWNQKepV4st7cQTpDdykYqw&key=AIzaSyA2CVFfCLdJu1y9pUfQJ7Osjexm5GAsQyo', function (response) {
+	client.get("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + idChannel + "&key=AIzaSyA2CVFfCLdJu1y9pUfQJ7Osjexm5GAsQyo", function (response) {
 		console.log(JSON.parse(response).items[0].statistics.subscriberCount);
 	});
 };
-
-showStat();
--- src/js/main.js
+++ src/js/main.js
@@ -12,8 +12,9 @@ var HttpClient = function () {
 }
 
 let client = new HttpClient();
+let idChannel = 'UCVswRUcKC-M35RzgPRv8qUg';
 let showStat = () => {
-	client.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCWNQKepV4st7cQTpDdykYqw&key=AIzaSyA2CVFfCLdJu1y9pUfQJ7Osjexm5GAsQyo', (response) => {
+	client.get(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${idChannel}&key=AIzaSyA2CVFfCLdJu1y9pUfQJ7Osjexm5GAsQyo`, (response) => {
 		console.log(JSON.parse(response).items[0].statistics.subscriberCount);
 	});
 }
@@ -23,22 +24,3 @@ let showStat = () => {
 
 
 
-// // 1. Создаём новый объект XMLHttpRequest
-// var xhr = new XMLHttpRequest();
-
-// // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
-// xhr.open('GET', 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCWNQKepV4st7cQTpDdykYqw&key=AIzaSyA2CVFfCLdJu1y9pUfQJ7Osjexm5GAsQyo', false);
-
-// // 3. Отсылаем запрос
-// xhr.send();
-
-// // 4. Если код ответа сервера не 200, то это ошибка
-// if (xhr.status != 200) {
-//   // обработать ошибку
-//   console.log( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
-// } else {
-// 	// вывести результат
-// 	var subsCount = JSON.parse(xhr.responseText ).items[0].statistics.subscriberCount;
-// 	document.querySelector('#count').innerText = subsCount;
-//    // responseText -- текст ответа.
-// }
